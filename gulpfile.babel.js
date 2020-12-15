bbf9233e3cd8d0937e1a674bf843ca42ef725010
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2020-12-15 11:03:47 -0700

    Add postcss dependencies; add minification and autoprefixer to CSS gulpfile task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,8 +6,10 @@ import rename from 'gulp-rename'
 
 //CSS
 import sass from '@selfisekai/gulp-sass';
+import cleanCss from 'gulp-clean-css';
 import stylelint from 'gulp-stylelint';
-import cleanCss from 'gulp-clean-css'
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
 
 //Both JS and CSS
 import sourcemaps from 'gulp-sourcemaps';
@@ -30,6 +32,7 @@ export const styles = () => {
     }))
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer]))
     .pipe(cleanCss({ compatibility:'*'}))
     .pipe(rename('style.css'))
     .pipe(sourcemaps.write())
